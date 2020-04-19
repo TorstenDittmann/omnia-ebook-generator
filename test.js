768cb44f0e556c88cc64af7b829a35da83d5623a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-04-19 17:38:39 +0200

    init repo

---
+++ test.js
@@ -0,0 +1,19 @@
+const EBook = require('./index');
+
+const ebook = new EBook({},
+    [
+        {
+            title: "About the author",
+            data: "<h2>Charles Lutwidge Dodgson</h2>"
+        },
+        {
+            title: "Down the Rabbit Hole",
+            data: "<p>Alice was beginning to get very tired...</p>"
+        }
+    ]);
+
+ebook.render();
+ebook.base64().then(data => {
+    console.log(data)
+});
+
